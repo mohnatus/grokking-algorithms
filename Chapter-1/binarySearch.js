215fab7a85dd9b1426eedacafa0878b50661f53a
mohnatus <furrycat.web@gmail.com>
2020-02-09 01:16:28 +0300

    Chapter 1

---
+++ Chapter-1/binarySearch.js
@@ -0,0 +1,22 @@
+function binarySearch(list, item) {
+  let low = 0;
+  let high = list.length - 1;
+
+  while (low <= high) {
+    let mid = Math.floor((low + high) / 2);
+    let guess = list[mid];
+
+    if (guess === item) return mid;
+
+    if (guess > item) high = mid - 1;
+    else low = mid + 1;
+  }
+
+  return undefined;
+}
+
+const sortedList = [1, 3, 5, 7, 9];
+
+console.log(binarySearch(sortedList, 1)); // 0
+console.log(binarySearch(sortedList, 7)); // 3
+console.log(binarySearch(sortedList, -1)); // undefined
